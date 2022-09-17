a8ea65bc7008dabf9acd384a7e71b6a5eda8dda1
viacheslavorlov <viacheslavorlov@gmail.com>
2022-09-17 17:33:54 +0300

    react-typescript - VLADILEN 1h 16min нужно прочитать про события в Typescript

--- src/components/CreateProduct.tsx
+++ src/components/CreateProduct.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const CreateProduct = () => {
+    const [value, setValue] = useState('')
+
+    const submitHandler = (e: React.FormEvent) => {
+        e.preventDefault();
+    }
+
+    const changeHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        setValue(event.currentTarget.value);
+    }
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <input
                 type="text"
                 placeholder="name of product"
                 className="py-2 px-4 border mb-2 w-full outline-0"
+                value={value}
+                onInput={changeHandler}
             />
             <button
                     className="py-2 px-4 border bg-yellow-400 hover:text-white hover:bg-green-500"

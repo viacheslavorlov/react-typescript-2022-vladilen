5335267d315fb3c6bcfc6ba33ed18040ba0dc88d
viacheslavorlov <viacheslavorlov@gmail.com>
2022-09-19 14:13:45 +0300

    react-typescript - VLADILEN 1h 35min нужно прочитать про события в Typescript и типирование

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,18 @@ import Loader from "./components/Loader";
 import Error from "./components/Error";
 import Modal from "./components/Modal";
 import CreateProduct from "./components/CreateProduct";
-import {useState} from "react";
+import {useContext} from "react";
 import {IProduct} from "./models";
+import {ModalContext} from "./components/ModalContext";
 
 
 
 function App() {
     const {error, loading, products, addProduct} = useAxios();
-    const [modal, setModal] = useState(true);
+    const {open, close, modal} = useContext(ModalContext)
 
     const onCreate = (product: IProduct) => {
-        setModal(false);
+        close()
         addProduct(product);
     }
 
@@ -24,6 +25,7 @@ function App() {
 
     return (
         <>
+
             <div className="container mx-auto max-w-2xl pt-5">
                 <h1 className="text-center text-5xl mb-3">TS + React.</h1>
 
@@ -31,12 +33,12 @@ function App() {
                 {error ? <Error error={error}/> : null}
                 {products.map((item) => <Product product={item} key={item.id}/>)}
 
-                {modal ? <Modal title="Create new product" onClose={() => setModal(false)}>
+                {modal ? <Modal title="Create new product" onClose={close}>
                     <CreateProduct onCreate={onCreate}/>
                 </Modal> : null}
                 {/*<Product product={ product[0] }/>*/}
                 {/*<Product product={ product[1] }/>*/}
-                <button onClick={() => setModal(true)}
+                <button onClick={open}
                     className="border border-black border-2 m-2 p-2 fixed bottom-5 right-5 rounded-full bg-red-600"
                 >Open modal</button>
             </div>
